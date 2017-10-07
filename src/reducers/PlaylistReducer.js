3a521b9d25d747155a75a3852bf1748917c2a5df
masato <hi@masato.io>
2017-10-06 21:22:04 -0700

    handle mutation

--- src/reducers/PlaylistReducer.js
+++ src/reducers/PlaylistReducer.js
@@ -1,7 +1,7 @@
 export default function(state = null, action) {
   switch (action.type) {
     case 'GET_ALL_SONGS':
-      return action.payload;
+      return [...action.payload];
   }
   return state;
 }
