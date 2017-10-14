9e8b356711adba236535e1156f327206347776cd
masato <hi@masato.io>
2017-10-14 14:04:08 -0700

    made credentail dynamic

--- server/env/credentials.js
+++ server/env/credentials.js
@@ -2,7 +2,8 @@ exports.client_id = 'f5405637034e4d1fbb01cfaf3c1616fe';
 exports.client_secret = '5d814b098b4e42c9ac393238370e6f97';
 exports.redirect_uri = 'http://127.0.0.1:3000/callback';
 // exports.redirect_homepage = 'http://127.0.0.1:8080'; // dev
-exports.redirect_homepage = 'http://127.0.0.1:3000'; // prod
+exports.redirect_homepage =
+  `http://127.0.0.1:${process.env.PORT}` || 'http://127.0.0.1:3000'; // prod
 exports.prod = false;
 exports.access_token = null;
 exports.refresh_token = null;
