64c0bfb271b10cba7b0a6be5a2dfa4eea7c70b7f
maximilian183 <max.kwok@gmail.com>
2017-10-14 13:40:26 -0700

    Add Host Dashboard with DeleteSong

---
+++ src/components/Host/HostPlayer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const HostPlayer = props => {
+  return (
+    <div>
+      <iframe
+        src={
+          'https://open.spotify.com/embed?uri=spotify:track:' + props.trackId
+        }
+        width="100%"
+        height="250"
+        frameBorder="0"
+        allowTransparency="true"
+      />
+    </div>
+  );
+};
+
+export default HostPlayer;
---
+++ src/components/Host/HostPlaylist.jsx
@@ -0,0 +1,127 @@
+// react & redux
+import React from 'react';
+import { connect } from 'react-redux';
+import { getSongs } from '../../actions/playlistActions';
+// ajax
+import axios from 'axios';
+// components
+import HostPlaylistEntry from './HostPlaylistEntry.jsx';
+import HostPlayer from './HostPlayer.jsx';
+// spotify
+import SpotifyWebApi from 'spotify-web-api-js';
+const spotifyApi = new SpotifyWebApi();
+// styled-components
+import styled from 'styled-components';
+
+@connect((store) => {
+  return {
+    access_token: store.AccountsReducer.access_token,
+    refresh_token: store.AccountsReducer.refresh_token,
+    player_id: store.AccountsReducer.player_id,
+    currentSong: store.songsReducer.currentSong
+  }
+})
+
+
+class HostPlaylist extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentSong: '',
+      currentUser: 'annonymous'
+    }
+  }
+
+  componentDidMount() {
+    this.props.dispatch(getSongs().bind(this));
+  }
+
+  removeSong(songId) {
+    axios
+      .delete(`${window.server}/song`, { params: { id: songId } })
+      .then(response => {
+        this.props.dispatch(getSongs().bind(this));
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    const {currentSong} = this.props;
+
+    if (currentSong) {
+      var render_Player = (
+        <HostPlayer trackId={currentSong.link.split('track/')[1]} />
+      )
+    }
+
+    const playListStyle = {
+      display: 'inline-block',
+      width: '90%',
+      float: 'right'
+    };
+    const playButtonStyle = {
+      width: '100%',
+      margin: '16px',
+      textAlign: 'center'
+    };
+
+    // define custom css
+    const PlaylistComponentWrap = styled.div`
+      background: #1c2137;
+      height: 100vh;
+      width: 100%;
+      overflow-y: scroll;
+      margin: 0 0 80px 0;
+    `;
+
+    const PlayerWrap = styled.div`
+      background: #1c2137;
+      height: 80px;
+      width: 900px;
+      position: fixed;
+      z-index: 100;
+      bottom: 0;
+      left: calc(-50vw + 50%);
+      right: calc(-50vw + 50%);
+      margin-left: auto;
+      margin-right: auto;
+      box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.5);
+    `;
+
+    return (
+      <PlaylistComponentWrap>
+        {/* PLAYLIST */}
+        <div style={playListStyle}>
+          {this.props.songs &&
+            this.props.songs.map((song, i) => {
+              return (
+                <HostPlaylistEntry
+                  index={i + 1}
+                  downVote={this.downVote}
+                  handlePlay={this.handlePlayButtonClick}
+                  upVote={this.upVote}
+                  Song={song}
+                  key={i}
+                  deleteSong={()=>{this.removeSong(song._id)}}
+                />
+              );
+            })}
+        </div>
+
+        <PlayerWrap>
+          {render_Player}
+        </PlayerWrap>
+      </PlaylistComponentWrap>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    songs: state.songsReducer.songs
+  };
+};
+
+export default connect(mapStateToProps, null)(HostPlaylist);
