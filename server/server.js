365549a8c17939f5979f03bc864195c6b41f8fec
masato <hi@masato.io>
2017-10-14 13:59:18 -0700

    adjusted PORT to dynamic

--- server/server.js
+++ server/server.js
@@ -60,6 +60,6 @@ app.get('/callback', (req, res) => {
 });
 
 // *** Server ***
-const server = app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, function() {
   console.log('Listening at http://localhost:3000');
 });
