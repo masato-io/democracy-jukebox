543c72281f52e2034a9af0ca751494fd12ae514b
masato <hi@masato.io>
2017-10-14 13:42:46 -0700

    optimized for foreman

--- db/config.js
+++ db/config.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://localhost/quince');
+mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect('mongodb://localhost/quince');
 
 mongoose.connection
   .once('open', () => {
-      console.log('Successfully connected');
+    console.log('Successfully connected');
   })
-  .on('error', (error) => {
-      console.warn('Connection error', error);
+  .on('error', error => {
+    console.warn('Connection error', error);
   });
 
 module.exports = mongoose;
