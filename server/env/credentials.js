a3c7491e73b4155b05e5ca9779af3fff625010c7
masato <hi@masato.io>
2017-10-17 16:09:12 -0700

    prod setting

--- server/env/credentials.js
+++ server/env/credentials.js
@@ -1,9 +1,9 @@
 exports.client_id = 'f5405637034e4d1fbb01cfaf3c1616fe';
 exports.client_secret = '5d814b098b4e42c9ac393238370e6f97';
-exports.redirect_uri = 'http://127.0.0.1:3000/callback'; // dev
-// exports.redirect_uri = 'http://peaceful-retreat-84338.herokuapp.com/callback'; // prod
-exports.redirect_homepage = 'http://127.0.0.1:8080'; // dev
-// exports.redirect_homepage = 'http://peaceful-retreat-84338.herokuapp.com/'; // prod
+// exports.redirect_uri = 'http://127.0.0.1:3000/callback'; // dev
+exports.redirect_uri = 'http://peaceful-retreat-84338.herokuapp.com/callback'; // prod
+// exports.redirect_homepage = 'http://127.0.0.1:8080'; // dev
+exports.redirect_homepage = 'http://peaceful-retreat-84338.herokuapp.com/'; // prod
 exports.prod = false;
 exports.access_token = null;
 exports.refresh_token = null;
