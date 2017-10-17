697410363b85c7cf3f4b04ecf05e05656324aeb5
masato <hi@masato.io>
2017-10-17 16:16:37 -0700

    https

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from './store';
 import App from './components/App.jsx';
 const app = document.getElementById('app');
 
-window.server = 'http://peaceful-retreat-84338.herokuapp.com'; // prod
+window.server = 'https://peaceful-retreat-84338.herokuapp.com'; // prod
 // window.server = 'http://127.0.0.1:3000'; // dev
 
 ReactDOM.render(
